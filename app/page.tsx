43f038dd9bfaf5d9f6de2351641f04c7cc04f5e0
Kathleen Therese Cruz <137745010+kathleentrc@users.noreply.github.com>
2025-11-22 22:08:02 +0800

    Added temp header, fixed landing styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <main className="px-10">
+      <main className="px-10 border-gray-200">
         <div className="flex flex-row items-center h-screen">
-          <div className="w-1/2 flex flex-col items-center p-5 gap-2">
+          <div className="w-1/2 flex flex-col items-center gap-2">
             <p className="text-sm font-albert font-bold tracking-tight text-red-800 mb-2">Explore</p>
             <p className="text-3xl font-nanum font-bold tracking-tight mb-3 px-24 text-center">POWER IN POETRY & PROSE</p>
             <p className="text-lg font-nanum font-regular tracking-tight text-center px-24 mb-3">We create a space where these important narratives can be shared, deepen awareness of social issues, and encourage meaningful action. </p>
             <button className="text-sm font-albert font-regular text-white px-7 py-3 mt-7 bg-slate-900 hover:bg-slate-700 transition tracking-tight">Start Reading</button>
           </div>
           
-          <div className="w-1/2 h-[700px] bg-gray-500 m-10">
-            {/* img placeholder */}
+          <div className="w-1/2 relative flex-1 h-full overflow-hidden">
+            <Image
+              src="/protest.png"
+              alt="Protest Image"
+              fill
+              className="object-cover mt-14 py-32"
+            />
           </div>
+
         </div>
 
         <div className="flex flex-col bg-slate-950 h-full -mx-10">
